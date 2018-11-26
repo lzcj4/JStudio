218b3e612e659e3ff87b8dda5da050fb92abff50
lzcj4 <lzcj4@163.com>
2018-11-26 22:35:31 +0800

    get request from context

--- src/main/java/com/jstudio/bookstore/controller/SettingController.java
+++ src/main/java/com/jstudio/bookstore/controller/SettingController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +16,11 @@ import java.util.Locale;
 @Controller
 public class SettingController {
     @PostMapping("/language")
-    public String list(HttpServletRequest request, String lang) {
+    public String list(HttpServletRequest request, @RequestParam("lang") String lang) {
+        HttpServletRequest request2 = ((ServletRequestAttributes) RequestContextHolder
+                .getRequestAttributes())
+                .getRequest();
+
         if (StringUtils.isEmpty(lang)) {
             lang = request.getParameter("lang");
         }
